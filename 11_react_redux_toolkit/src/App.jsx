337a5d872683034267744b6e25c9b9cbfdd328ec
biswass101 <biswassnaeemcse@gmail.com>
2025-02-25 18:49:52 +0600

    fethced data using redux-toolkit

--- 11_react_redux_toolkit/src/App.jsx
+++ 11_react_redux_toolkit/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CounterView from './features/counter/CounterView'
 import CounterView2 from './features/counter_2/CounterView2'
+import PostsView from './features/posts/PostsView'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <h1>Counter App</h1>
       <CounterView/>
       <CounterView2/>
+      <PostsView/>
     </div>
   )
 }
--- 11_react_redux_toolkit/src/app/store.js
+++ 11_react_redux_toolkit/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import counterReducer2 from "../features/counter_2/counter2Slice";
+import postReducer from '../features/posts/postSlice'
 
 const store = configureStore({
     reducer: {
         counter : counterReducer, //mainly a reducer
-        counter2 : counterReducer2
+        counter2 : counterReducer2,
+        posts: postReducer
     }
 })
 
